4bf009510a680d4d84dd4271894d92f8f3786439
Elysiario <elysiario@gmail.com>
2023-07-05 14:19:32 +0100

    arquivos novos

--- Banco de dados com Python e SQlite/prodv1.py
+++ Banco de dados com Python e SQlite/prodv1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -60,3 +61,67 @@ print("Job Concluído com Sucesso")
 
 
 
+=======
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Demonstração Prática 4 - Criação de Pipeline de Extração, Limpeza, Transformação e Enriquecimento de Dados  Versão 1
+
+# In[ ]:
+
+
+#Imports
+import sqlite3
+import csv
+
+
+# In[ ]:
+
+
+# Cria um novo banco de dados
+conn = sqlite3.connect('dsadb.db')
+
+
+# In[ ]:
+
+
+#Cria uma tabela para armazenar os dados de produção de alimentos
+conn.execute('''CREATE TABLE producao (
+produto TEXT, quantidade INTEGER, preco_medio REAL, receita_total REAL)''')
+
+
+# In[ ]:
+
+
+#Grava e fecha a conexão
+
+conn.commit()
+conn.close()
+
+
+# In[ ]:
+
+
+#Abre arquivo csv com os dados de produção de alimentos 
+with open('producao_alimentos.csv', 'r') as file:
+    #Cria um leitor de csv para ler o arquivo
+    reader = csv.reader(file)
+    #Conecta ao banco de dados
+    conn = sqlite3.connect('dsabd.db')
+    #pula a primeira linha, que contém os cabeçalhos das colunas 
+    next(reader)
+    #Insere cada linha do arquivo na tabela de banco de dados
+    for row in reader:
+        conn.execute('INSERT INTO producao (produto, quantidade, preco_medio, receita_total) VALUES (?,?,?,?)', row)
+                     
+    conn.commit()
+    conn.close
+print("Job Concluído com Sucesso")
+
+
+# In[ ]:
+
+
+
+
+>>>>>>> 6d146f54a6157215f8868ec878ce20a52215a4a5
